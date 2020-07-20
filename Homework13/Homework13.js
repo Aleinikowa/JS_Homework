eb87a20f1e04d37e8d2cb83e6da4fec1c59c0680
Aleinikowa <polinaalejnikowa@gmail.com>
2020-07-20 21:12:25 +0300

    remade homework

--- Homework13/Homework13.js
+++ Homework13/Homework13.js
@@ -2,13 +2,11 @@
 var array = ['Polina', 'Juli', 'Max', 'Vasya'];
 
 function changeNames(arr) {
-    var newArray = [];
-
-    arr.map(function (names) {
+    
+    var newArray = arr.map(function(names) {
         var object = {};
         object.name = names;
-        newArray.push(object);
-        return newArray;
+        return object;
     });
 
     return newArray;
@@ -24,30 +22,29 @@ function stringFromArray(arr) {
 
     var result = arr.reduce(function(element, number) {
         return element + ' : ' + number;
-    }, ' ');
+    }, 'Текущее время ');
 
-    return 'Текущее время ' + result;
+    return result;
 }
 
 stringFromArray(array);
 
 
 // Задание 3:
-
 var string = 'полина';
 
 function howManyVowels(str) {
     var arrayVowels = ['а', 'е', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
     var arrayFromString = str.toLowerCase().split('');
+    var amount = 0;
 
-    var vowels = arrayFromString.reduce(function(sum, item) {
+    var vowels = arrayFromString.forEach(function(item) {
         if (arrayVowels.indexOf(item) !== -1) {
-            sum ++;
+            amount ++;
         }
-        return sum;
-    }, 0); 
+    });
 
-    return vowels;
+    return amount;
 }
 
 howManyVowels(string);
@@ -63,9 +60,9 @@ function splitSentence(sentence) {
         var newSentence = item.trim();
 
         splitArray.forEach(function(elem) {
-            var string = elem.replace(/[? ,]/g,"");
+            var string = elem.split(/[!... ?,  ]+/).join('');
             var stringLength = string.length;
-            console.log( newSentence + ': ' + ' Letters quantity is: ' + stringLength );
+            console.log( newSentence + ': ' + ' Letters quantity is: ' + stringLength);
         });
     });
 }
@@ -74,35 +71,3 @@ splitSentence('Привет, студент! Студент... Как дела, 
 // Привет, студент: Letters quantity is: 13
 // Студент: Letters quantity is: 7
 // Как дела, студент: Letters quantity is: 14
-
-
-//Задание 5 *:
-var text = 'Привет всем! Меня зовут Сара. Еще раз, привет всем всем! Также отдельный привет моим друзьям.';
-
-function splitSentence(sentence) {
-    var arrayFromText = sentence.toLowerCase().split(/[, !.]+/);
-    var objectCount = {};
-
-    arrayFromText.forEach(function(item) {
-        objectCount[item] = (objectCount[item] || 0) + 1;
-    });
-
-    var arraySort = [];
-
-    for (var key in objectCount) {
-        arraySort.push([key, objectCount[key]]);
-    }
-
-    arraySort.sort(function(a, b) {
-        return b[1] - a[1];
-    });
-
-    var finalArray = arraySort[0];
-    var finalWord = finalArray[0];
-    var finalValue = finalArray[1];
-
-    return 'Максимальное число повторений у слова "' + finalWord + '" - ' + finalValue;
-
-}
-
-splitSentence(text);
