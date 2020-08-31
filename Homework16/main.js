eb0c22032f07ec7c05c13b4e233eccd472a558f1
Polina <polinaalejnikowa@gmail.com>
2020-08-31 21:10:24 +0300

    fix homework

--- Homework16/main.js
+++ Homework16/main.js
@@ -1,35 +1,89 @@
 var btn = document.getElementById('btnJS'),
     blockBtn = document.getElementsByClassName('btn')[0],
-    blockUsers = document.getElementsByClassName('users')[0];
+    blockUsers = document.getElementById('users'),
+    anyUser = document.getElementById('anyUser'),
+    dataUsers;
 
-btn.addEventListener('click', addUsers);    
+btn.addEventListener('click', loadData);    
 
 window.onload = function() {
-    if ( !(localStorage.getItem('arrayUsers')) ) {
-        var xhr = new XMLHttpRequest;
-        xhr.open('GET', 'https://reqres.in/api/users?page=2');
-        xhr.send();
-    
-        xhr.onload = function() {
-            var status = +String(this.status)[0];
-    
-            if (status == 2) {
-                dataUsers = JSON.parse(this.response).data;
-                localStorage.setItem('arrayUsers', JSON.stringify(dataUsers));
-            }
-            if (status == 4) {
-                err();
-            }
-        }
+    if ((localStorage.getItem('arrayUsers')) ) {
+        addInputs();
+    }
+}
 
-        xhr.onerror = function() {
+function loadData() {
+    var xhr = new XMLHttpRequest;
+    xhr.open('GET', 'https://reqres.in/api/users?page=2');
+    xhr.send();
+
+    xhr.onload = function() {
+        var status = +String(this.status)[0];
+
+        if (status == 2) {
+            dataUsers = JSON.parse(this.response).data;
+            localStorage.setItem('arrayUsers', JSON.stringify(dataUsers));
+            addInputs();
+        }
+        if (status == 4) {
             err();
         }
-    } 
+    }
+
+    xhr.onerror = function() {
+        err();
+    }
+}
+
+function addInputs() {
+    dataUsers = localStorage.getItem('arrayUsers');
+    var data = JSON.parse(dataUsers);
+
+    blockBtn.style.display = 'none';
+    blockUsers.style.display = 'block';
+
+    data.forEach(function(obj,counter) {
+        var i = ++counter;
+        blockUsers.innerHTML += '<input type="radio" name="dataUser" id="' + i + '"class="users__radio">';     
+        blockUsers.innerHTML += '<label for="' + i + '" class="users__label">User  ' + i + ' </label>'; 
+
+        var first = document.getElementsByTagName('input')[0],
+            item = first.getAttribute('id');
+        
+        first.setAttribute('checked', true);    
+        anyUser.classList.add('anyUser');
+        anyUser.innerHTML = createHtml(data, item);
+    })
+
+    blockUsers.onclick = function(event) {
+        var target = event.target;
+        if (target.tagName == 'LABEL') {
+            var item = target.htmlFor;
+            anyUser.classList.add('anyUser');
+            anyUser.innerHTML = createHtml(data, item);
+        }
+    }
+}
+
+function createHtml(array, counter) {
+    var object =  array[--counter],
+        fNameUser = object.first_name,
+        lNameUser = object.last_name,
+        avatarUser = object.avatar,
+        emailUser = object.email,
+        idUser = object.id,
+        html = '<img src=" ' + avatarUser + ' " alt="avataruser' + --counter + ' ">' +
+            '<h2><span>First Name:</span> ' + fNameUser + '</h2>' +
+            '<h2><span>Last Name:</span> ' + lNameUser + '</h2>' +
+            '<h3><span>email:</span> ' + emailUser + '</h3>' +
+            '<h3><span>id:</span> ' + idUser + '</h3>';
+
+    return html;
 }
 
 function err () {
-    var blockErr = document.getElementsByClassName('error')[0];
+    var blockErr = document.getElementById('error');
+    blockErr.classList.add('error');
     blockErr.style.display = 'block';
 
     function showEror() {
@@ -37,34 +91,3 @@ function err () {
     }
     setTimeout(showEror, 1000); 
 }
-
-function addUsers() {
-    var dataUsers = localStorage.getItem('arrayUsers'),
-        data = JSON.parse(dataUsers);
-
-    blockBtn.style.display = 'none';
-    blockUsers.style.display = 'block';
-
-    data.forEach(function(object, counter) {
-        var fNameUser = object.first_name,
-            lNameUser = object.last_name,
-            avatarUser = object.avatar,
-            emailUser = object.email,
-            idUser = object.id,
-            i = ++counter,
-            divUser = document.createElement('div');
-        
-        blockUsers.innerHTML += '<input type="radio" name=FOLDER id="user' + i + '"class="users__radio">';     
-        blockUsers.innerHTML += '<label for="user' + i +'">User  ' + i + '</label>'; 
-        divUser.classList.add('users__div');
-        divUser.innerHTML += '<img src=" ' + avatarUser + ' " alt="avataruser' + i + ' ">';
-        divUser.innerHTML += '<h2><span>First Name:</span> ' + fNameUser + '</h2>';
-        divUser.innerHTML += '<h2><span>Last Name:</span> ' + lNameUser + '</h2>';
-        divUser.innerHTML += '<h3><span>email:</span> ' + emailUser + '</h3>';
-        divUser.innerHTML += '<h3><span>id:</span> ' + idUser + '</h3>';
-        blockUsers.appendChild(divUser);
-    })
-   document.getElementById('user1').setAttribute('checked', true);
-}
-
- 
